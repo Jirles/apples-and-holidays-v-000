9586f01bf67f9b602361c0ca94ed6dc93b9e174b
Emily Jirles <emily.jirles@gmail.com>
2018-04-30 02:11:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
           supplies_str << "#{item.capitalize}, " 
         end 
       end
-    puts "  #{holiday.to_s.sub("_", " ").capitalize}: #{supplies_str}"
+    puts "  #{holiday.to_s.sub("_", " ").sub()}: #{supplies_str}"
     end
   end
 end
